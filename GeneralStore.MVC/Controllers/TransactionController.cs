64adf98df0cefec9fb2e67adb5314640ccc0e359
Samir Omerani <omegraph@gmail.com>
2021-10-10 21:32:40 -0400

    Edit Transaction was added to TransactionController

--- GeneralStore.MVC/Controllers/TransactionController.cs
+++ GeneralStore.MVC/Controllers/TransactionController.cs
@@ -38,10 +38,79 @@ namespace GeneralStore.MVC.Controllers
         }
         // POST: Transaction/Create
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Create(CreateTransactionViewModel viewModel)
         {
             return View(viewModel);
         }
+
+        // GET: Transaction/Delete/{id}
+        public ActionResult Delete(int id)
+        {
+            Transaction transaction = _db.Transactions.Find(id);
+            if (transaction == null)
+            {
+                return HttpNotFound();
+            }
+
+
+            return View(transaction);
+        }
+        // POST: Transaction/Delete/{id}
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(Transaction transaction)
+        {
+            // DO OUR DB STUFF
+            //***********************************
+            if (/* something went wrong */ 1 = 1)
+            {
+                // return custom view
+                ViewData["ErrorMessage"] = "Couldn't delete your transaction";
+                return View(transaction);
+            }
+            return RedirectToAction("Index" );
+        }
+        // GET: Transaction/Edit/{id}
+        public ActionResult Edit(int id)
+        {
+            Transaction transaction = _db.Transactions.Find(id);
+
+            if (transaction == null)
+            {
+                return HttpNotFound();
+            }
+            ViewData["Customers"] = _db.Customers.Select(customer => new SelectListItem
+            {
+                Text = customer.FirstName + " " + customer.LastName,
+                Value = customer.CustomerID.ToString()
+            });
+
+            ViewData["Products"] = _db.Products.Select(product => new SelectListItem
+            {
+                Text = product.Name,
+                Value = product.ProductId.ToString()
+            });
+
+            return View(transaction);
+        }
+        //Post: Transaction/Edit/{id}
+        public ActionResult Edit(Transaction transaction)
+        {
+            ViewData["Customers"] = _db.Customers.Select(customer => new SelectListItem
+            {
+                Text = customer.FirstName + " " + customer.LastName,
+                Value = customer.CustomerID.ToString()
+            });
+
+            ViewData["Products"] = _db.Products.Select(product => new SelectListItem
+            {
+                Text = product.Name,
+                Value = product.ProductId.ToString()
+            });
+            return View(transaction);
+        }
+
 
     }
 }
